fbe907ac5ada5be147bd98929bf1408d49a9c423
Basil ALBADER <baalbade@student.42.fr>
2023-10-04 10:25:11 +0200

    WIP

--- ref/parse_map.h
+++
@@ -1,7 +0,0 @@
-#ifndef PARSE_MAP_H
-# define PARSE_MAP_H
-# include "main.h"
-
-int	parse_input(t_data *data, char *filename);
-
-#endif
--- ref/skybox.h
+++
@@ -1,7 +0,0 @@
-#ifndef SKYBOX_H
-# define SKYBOX_H
-# include "main.h"
-
-void	render_skybox(t_data *d);
-
-#endif
