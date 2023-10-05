8753ec67b2b383ca77348e556b367c5134560470
Basil ALBADER <baalbade@student.42.fr>
2023-10-05 11:05:10 +0200

    cub3d.h

---
+++ inc/cub3d.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/05 11:01:17 by baalbade          #+#    #+#             */
+/*   Updated: 2023/10/05 11:01:24 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "define.h"
+# include "structures.h"
+
+# include <unistd.h>
+# include <stdio.h>
+# include <math.h>
+# include <stdlib.h>
+# include <stdbool.h>
+
+#endif	
