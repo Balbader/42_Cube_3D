ee0869a8b2ad07c5d221ace58817b6f6b33039b7
Basil ALBADER <baalbade@student.42.fr>
2023-10-07 15:55:14 +0200

    WIP

--- inc/structures.h
+++ inc/structures.h
@@ -13,65 +13,104 @@
 #ifndef STRUCTURES_H
 # define STRUCTURES_H
 
-# include "enum.h"
+typedef int			t_bool;
 
-# define WIDTH 1024
-// # define WIDTH 683
-# define HEIGHT 384
-
-typedef struct s_data
+typedef enum		e_side
 {
-	char	**map;
-	int		map_height;
-	int		map_width;
-	char	*north_texture;
-	char	*south_texture;
-	char	*west_texture;
-	char	*east_texture;
-	int		floor_rgb;
-	int		ceiling_rgb;
-	int		player_x;
-	int		player_y;
-	char	player_dir;
-	float	player_dir_radian;
-}				t_data;
+	N,
+	W,
+	S,
+	E
+}					t_side;
 
-typedef struct s_list
+typedef struct		s_mlx
 {
-	char			*line;
-	int				line_size;
-	int				type;
-	struct s_list	*next;
-}				t_list;
+	void			*ptr;
+	void			*win;
+}					t_mlx;
 
-typedef struct s_ray
+typedef struct		s_dda
 {
-	float	dist;
-	double	angle;
-	int		type;
-	double	end_pos[2];
-}				t_ray;
+	double	side_dist[2];
+	double	delta_dist[2];
+	double	wall_dist;
+	int		map_pos[2];
+	int		step[2];
+}					t_dda;
 
-typedef struct s_img
+typedef struct		s_texdata
 {
-	void	*img;
-	int		*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
+	double	wall_dist;
+	double	wall_x;
+	int		line_height;
+	t_side	side;
+}					t_texdata;
+
+typedef struct		s_spritedata
+{
+	double	transform[2];
+	int		sprite_x;
+	int		sprite_size;
+	int		index;
+}					t_spritedata;
+
+typedef struct		s_ctrls
+{
+	t_bool	w;
+	t_bool	a;
+	t_bool	s;
+	t_bool	d;
+	t_bool	left;
+	t_bool	right;
+}					t_ctrls;
+
+typedef struct		s_img
+{
+	char	*filename;
+	void	*ptr;
 	int		width;
 	int		height;
-}				t_img;
+	char	*data;
+	int		bpp;
+	int		size_line;
+	t_bool	endian;
+}					t_img;
 
-typedef struct s_mlx
+typedef struct		s_sprite
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
-	t_img	pict[MAX_IMG];
-	double	player[MAX_POS];
-	double	delta[2];
-	t_ray	rayon[WIDTH];
-	t_data	*data;
-}				t_mlx;
+	int		pos[2];
+	double	distance;
+	t_img	texture;
+}					t_sprite;
+
+typedef struct		s_world
+{
+	t_mlx			mlx;
+	int				fd;
+	t_bool			error;
+	int				scr_width;
+	int				scr_height;
+	int				map_width;
+	int				map_height;
+	char			*raw_map;
+	char			**char_map;
+	int				**map;
+	int				color_floor;
+	int				color_ceiling;
+	t_img			textures[4];
+	t_img			texture_sprite;
+	int				nb_sprites;
+	t_sprite		*sprites;
+	t_img			screen;
+	t_ctrls			ctrls;
+	int				nb_pixels;
+	double			*depth_buffer;
+	double			pos[2];
+	double			dir[2];
+	double			cam_plane[2];
+	t_side			cam_dir;
+	t_bool			save;
+	int				fd_save;
+}					t_world;
 
 #endif	
