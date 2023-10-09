2838d657a2a34777f7dc63375e16fcdaf9f53764
Basil ALBADER <baalbade@student.42.fr>
2023-10-09 16:24:47 +0200

    folder deletion

--- outsiders/cub1/renderer.h
+++
@@ -1,9 +0,0 @@
-#ifndef RENDERER_H
-# define RENDERER_H
-# include "main.h"
-
-void		render_walls(t_data *data);
-void		render_floor(t_data *data);
-void		render_sprites(t_data *data);
-
-#endif
--- outsiders/cub1/sound.h
+++
@@ -1,6 +0,0 @@
-#ifndef SOUND_H
-# define SOUND_H
-
-int		start_music(void);
-
-#endif
