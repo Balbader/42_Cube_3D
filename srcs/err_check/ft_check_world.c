0348cc9417759365fd97acdfe8570e5458b6a1b9
Basil ALBADER <baalbade@student.42.fr>
2023-10-09 11:39:15 +0200

    ft_check_world.c

--- srcs/err_check/ft_check_world.c
+++ srcs/err_check/ft_check_world.c
@@ -15,20 +15,19 @@
 void	ft_check_world(t_world *world)
 {
 	if (!world->scr_width)
-		parsing_error(world, "Resolution must be defined before the map", 0);
+		ft_check_parsing(world, "Resolution must be defined before the map", 0);
 	else if (!world->color_floor)
-		parsing_error(world, "Floor color must be defined before the map", 0);
+		ft_check_parsing(world, "Floor color must be defined before the map", 0);
 	else if (!world->color_ceiling)
-		parsing_error(world, "Ceiling color must be defined before the map", 0);
+		ft_check_parsing(world, "Ceiling color must be defined before the map", 0);
 	else if (!world->textures[N].filename)
-		parsing_error(world, "North texture must be defined before the map", 0);
+		ft_check_parsing(world, "North texture must be defined before the map", 0);
 	else if (!world->textures[W].filename)
-		parsing_error(world, "West texture must be defined before the map", 0);
+		ft_check_parsing(world, "West texture must be defined before the map", 0);
 	else if (!world->textures[S].filename)
-		parsing_error(world, "South texture must be defined before the map", 0);
+		ft_check_parsing(world, "South texture must be defined before the map", 0);
 	else if (!world->textures[E].filename)
-		parsing_error(world, "East texture must be defined before the map", 0);
+		ft_check_parsing(world, "East texture must be defined before the map", 0);
 	else if (!world->texture_sprite.filename)
-		parsing_error(world, "Sprite texture must be defined before the map",
-			0);
+		ft_check_parsing(world, "Sprite texture must be defined before the map", 0);
 }
