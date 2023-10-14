f3091fb640aab46ae13fbaa4ce3e7c05f0f9d3e4
Basil ALBADER <baalbade@student.42.fr>
2023-10-14 13:45:28 +0200

    raycasting functions

---
+++ new/srcs/raycasting/ft_horizontal_check.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_horizontal_check.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/14 13:30:59 by baalbade          #+#    #+#             */
+/*   Updated: 2023/10/14 13:31:01 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+double	ft_horizontal_check(t_mlx *mlx, double re, double end_pos[2])
+{
+	double	ray[2];
+	double	offset[2];
+	float	atan;
+
+	atan = -1 / tan(ra);
+	if (ra < M_PI)
+	{
+		ray[1] = (((int)mlx->player[Y_PIXEL] >> 6) << 6) - 0.0002;
+		ray[X] = (mlx->player[Y_PIXEL] - ray[1]) * atan + mlx->player[X_PIXEL];
+		offset[1] = -64;
+		offset[X] = -offset[1] * atan;
+	}
+	if (ra > M_PI)
+	{
+		ray[1] = (((int)mlx->player[Y_PIXEL] >> 6) << 6) + 64;
+		ray[X] = (mlx->player[Y_PIXEL] - ray[1]) * atan + mlx->player[X_PIXEL];
+		offset[1] = 64;
+		offset[X] = -offset[1] * atan;
+	}
+	if (ra == 0 || ra == M_PI)
+		return (1000000);
+	return (ft_horizontal_while(mlx, ray, offset, end_pos));
+}
