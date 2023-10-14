7eefef07049866aa77f1970391ef3d7d609ad6bc
Basil ALBADER <baalbade@student.42.fr>
2023-10-14 13:45:21 +0200

    WIP

--- new/inc/cub3d.h
+++ new/inc/cub3d.h
@@ -13,10 +13,7 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
-/*************/
-/* err_check */
-/*************/
-
+/****************************************************************** err_check */
 /* duplicates_err */
 void	ft_check_cardinal_duplicates(t_list **list, int *dup, char *str, int i);
 void	ft_check_color_duplicates(t_list **list, int *dup, char *str, int i);
@@ -35,7 +32,7 @@ int		ft_check_cardinal_syntax(char *str);
 int		ft_check_fc_syntax(char *str);
 void	ft_check_last_element(t_list **list);
 void	ft_check_list_syntax(t_list **list);
-void	ft_check_splitted_map(t_list **list)
+void	ft_check_splitted_map(t_list **list);
 
 /* textures_err */
 int		ft_check_texture_size(char *path, void *mlx);
@@ -44,9 +41,7 @@ void	ft_check_textures_extensions(t_data **data);
 void	ft_close_dir_error_exit(t_data **data, int *fd);
 void	ft_close_error_exit(t_data **data, int *fd);
 
-/*********/
-/* clear */
-/*********/
+/********************************************************************** clear */
 void	ft_clear_all_exit(t_data **data, t_list **list, char *msg);
 void	ft_clear_data(t_data **data);
 void	ft_clear_data_exit(t_data **data, char *msg);
@@ -57,15 +52,16 @@ void	ft_clear_list_free_line_exit(t_list **list, char *line);
 void	ft_clear_list_syntax_exit(t_list **list, int i, char *line, char *msg);
 void	ft_clear_mlx_fd_data_exit(t_data **data, void *mlx, int *fd, char *msg);
 
-/********/
-/* free */
-/********/
+/*********************************************************************** free */
 void	ft_free_array(char **arr);
 void	ft_free_mlx(t_mlx *mlx);
 void	ft_free_texture(t_mlx *mlx);
 
-
-
-
+/***************************************************************** raycasting */
+double	ft_horizontal_check(t_mlx *mlx, double re, double end_pos[2]);
+double	ft_horizontal_while(t_mlx *mlx, double ray[2], double offset[2]);
+double	ft_vertical_check(t_mlx *mlx, double ra, double end_pos[2]);
+double	ft_vertical_while(t_mlx *mlx, double rayon[2], \
+		double offset, double end_pos[2]);
 
 #endif // !CUB3D_H
