38b45c7be5197f314e2908b4865aeb81eb665d0a
Basil ALBADER <baalbade@student.42.fr>
2023-10-16 07:37:41 +0200

    fill function

---
+++ new/srcs/fill/ft_fill_data.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fill_data.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/16 07:04:01 by baalbade          #+#    #+#             */
+/*   Updated: 2023/10/16 07:04:06 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void	ft_fill_data(t_data **data, t_list **list)
+{
+	ft_init_data(data, list);
+	ft_fill_texture(data, list, &(*data)->north_texture, "NO ");
+	ft_fill_texture(data, list, &(*data)->south_texture, "SO ");
+	ft_fill_texture(data, list, &(*data)->west_texture, "WE ");
+	ft_fill_texture(data, list, &(*data)->east_texture, "EA ");
+	ft_fill_rgb(data, list, &(*data)->floor_rgb, "F ");
+	ft_fill_rgb(data, list, &(*data)->ceiling_rgb, "C ");
+	ft_get_map_size(data, list);
+	ft_init_map(data, list);
+	ft_fill_map(data, list);
+	ft_get_player_infos(data);
+}
